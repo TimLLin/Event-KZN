3e8a53b57e547c8667371e842b8c98fac98e695e
jrebeca <timurkalimullin@MacBook-Pro-Timur.local>
2021-06-11 23:42:58 +0500

    v 0.0.0

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, Filters
-from telegram import ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, CallbackQueryHandler, Filters
+from telegram import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 import logging
 import mainf
 
@@ -9,21 +9,55 @@ logger = logging.getLogger(__name__)
 
 updater = Updater(mainf.token_t, use_context=True)
 
+SELECTING_LEVEL, CONCERT_LEVEL = map(chr,range(2))
+END = ConversationHandler.END
 #########################################
 def start(update,context):
     start_message = 'Приветствую {},\nМоя цель помочь Вам найти тусу вам на вечер.\n\nВыберите заинтересовавшую вас категорию'.format(update.message.chat.first_name)
     context.bot.send_message(update.effective_chat.id, start_message, reply_markup=main_menu_keyboard())
+    return SELECTING_LEVEL
 
+def concert_recomendation(update,context):
+    context.bot.send_message(update.effective_chat.id,"Рекомендуемые Вам концерты", reply_markup=concert_recomendation_keyboard())
+    return CONCERT_LEVEL
+
+def concert_info(update,context):
+    message = mainf.chosen_concert()
+    context.bot.send_message(update.effective_chat.id, message[2])
+
+def stop(update, context):
+    context.bot.send_message(update.effective_chat.id, "Ну и пошел ты")
+    return END
 #########################################
 def main_menu_keyboard():
     markup = [[KeyboardButton(text='Театр🎭'), KeyboardButton(text='Кино🎬')],
               [KeyboardButton(text='Концентры🎶'), KeyboardButton(text='Выставки🖼')],
               [KeyboardButton(text='Другое🧐')]]
-    return ReplyKeyboardMarkup(markup, resize_keyboard = True)
+    return ReplyKeyboardMarkup(markup, resize_keyboard=True)
+
+def concert_recomendation_keyboard():
+    i = 0
+    dic = mainf.bulka()
+    keyboard = [[InlineKeyboardButton(list(dic)[i], callback_data=dic.get(list(dic)[i])), InlineKeyboardButton(list(dic)[i+1], callback_data=dic.get(list(dic)[i+1]))],
+                [InlineKeyboardButton(list(dic)[i+2], callback_data=dic.get(list(dic)[i + 3])), InlineKeyboardButton(list(dic)[i + 3], callback_data=dic.get(list(dic)[i + 3]))],
+                [InlineKeyboardButton(list(dic)[i + 4], callback_data=dic.get(list(dic)[i + 4]))],
+                [InlineKeyboardButton('Дальше', callback_data='next')]]
+
+    return InlineKeyboardMarkup(keyboard)
+
+#def concert_recommedation_edit_keyboard()
 
 #########################################
 def main():
-    updater.dispatcher.add_handler(CommandHandler('start',start))
+    updater.dispatcher.add_handler(ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            SELECTING_LEVEL: [MessageHandler(Filters.regex('Концентры🎶'), concert_recomendation)],
+            CONCERT_LEVEL : [CallbackQueryHandler(concert_info,pattern="/concert/2049288/")]
+
+        },
+        fallbacks=[CommandHandler('start', start)]
+    ))
 
     updater.start_polling()
     updater.idle()
--- mainf.py
+++ mainf.py
@@ -1,4 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
 
 
 token_a = '1844668199:AAGqVvqCfa5a60vcEQ_Cm9dNMtuvRslDAXg'
 token_t = '1777347114:AAEow6W4NmD1r6OYCmKCtjMRth7olY9hOHk'
+
+def bulka():
+    muk = []
+    duk = []
+    response = requests.get('https://www.afisha.ru/kazan/schedule_concert/s-biletami/')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    suska = soup.find_all('a', class_="_1F19s")
+    kek = soup.find_all('a', class_='_1F19s')
+    for data in suska:
+        if soup.find_all('a', href=True) or soup.find_all('h3', class_='heHLK'):
+            muk.append(data.text)
+            duk.append(data['href'])
+    dic = {key:value for key,value in zip(muk,duk)}
+    return dic
+
+
+def chosen_concert():
+    muk = []
+    response = requests.get('https://www.afisha.ru/concert/2049288/')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    adress = soup.find_all('div', class_="unit__col unit__col_oneandhalf")
+    date = soup.find_all('h2', class_='_2YgOJ')
+    image = soup.find_all('img', class_='EVCML')
+    for data in adress:
+        if soup.find_all('div', class_='unit__col unit__col_oneandhalf'):
+            muk.append(data.text)
+    for data in date:
+        if soup.find_all('h2', class_='_2YgOJ'):
+            muk.append(data.text)
+    for data in image:
+        muk.append(data['src'])
+
+    return muk
+
+
+chosen_concert()
+bulka()
+
