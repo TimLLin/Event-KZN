2248116803991bf62d161b506f5e60ec7dfc58e5
jrebeca <timurkalimullin@MacBook-Pro-Timur.local>
2021-06-12 13:46:06 +0500

    v 0.0.1

--- bot1.py
+++ bot1.py
@@ -30,20 +30,79 @@ def start_answer(a):
         for i in range(4):
             bt = types.InlineKeyboardButton(list(dic)[i], callback_data=dic.get(list(dic)[i]))
             markup_inline.add(bt)
+        bt_b = types.InlineKeyboardButton('➡️', callback_data='next')
+        markup_inline.add(bt_b)
         bot.send_message(a.message.chat.id, "Рекомендуемые Вам концерты", reply_markup=markup_inline)
 
-    if a.data in list(mainf.concert_data.values()):
-        mainf.querydata = a.data
-        mainf.chosen_photo(mainf.querydata)
-        bot.send_photo(a.message.chat.id, open('img.jpg', 'rb'),mainf.chosen_concert(mainf.querydata))
+
+    elif a.data in list(mainf.concert_data.values()):
+            try:
+                mainf.querydata = a.data
+                mainf.chosen_photo(mainf.querydata)
+                bot.send_photo(a.message.chat.id, open('img.jpg', 'rb'), mainf.chosen_concert(mainf.querydata))
+            except AttributeError:
+                bot.send_message(a.message.chat.id, mainf.chosen_concert(mainf.querydata))
+
+    elif a.data == 'next':
+        mainf.event_index += 5
+        markup_inline = types.InlineKeyboardMarkup()
+        dic = mainf.bulka()
+        for i in range(4):
+            if mainf.event_index+i < len(dic):
+                bt = types.InlineKeyboardButton(list(dic)[i+mainf.event_index], callback_data=dic.get(list(dic)[i + mainf.event_index]))
+                markup_inline.add(bt)
+            elif mainf.event_index+i > len(dic):
+                markup_inline1 = types.InlineKeyboardMarkup()
+                bt_1 = types.InlineKeyboardButton("Вернуться в главное меню", callback_data="back_main_menu")
+                markup_inline1.add(bt_1)
+                bot.send_message(a.message.chat.id, "Конец списка ремондаций. Если вы не нашли то что искали, перейдите в главное меню и воспользуйтесь рекомендациями в других категориях", reply_markup=markup_inline1 )
 
 
-    if a.data in list(mainf.concert_data.values()) and a.data != mainf.querydata:
-        bot.edit_message_media(a.message.chat.id, open('img.jpg', 'rb'))
-        bot.edit_message_text(a.message.chat.id, mainf.chosen_concert(mainf.querydata))
+        if mainf.event_index + 4 < len(dic):
+            bt_b = types.InlineKeyboardButton("➡️", callback_data='next')
+            bt_p = types.InlineKeyboardButton("⬅️", callback_data='previous')
+            markup_inline.add(bt_p, bt_b)
+        else:
+            bt_p = types.InlineKeyboardButton("⬅️", callback_data='previous')
+            markup_inline.add(bt_p)
+        bot.edit_message_reply_markup(a.message.chat.id, a.message.message_id, reply_markup=markup_inline)
+
+    elif a.data == "previous":
+        mainf.event_index -= 5
+        markup_inline_back = types.InlineKeyboardMarkup()
+        dic = mainf.bulka()
+        for i in range(4):
+            if mainf.event_index + i > 0:
+                bt = types.InlineKeyboardButton(list(dic)[i + mainf.event_index],
+                                            callback_data=dic.get(list(dic)[i + mainf.event_index]))
+                markup_inline_back.add(bt)
+            elif mainf.event_index + i == 0:
+                bt = types.InlineKeyboardButton(list(dic)[i + mainf.event_index],
+                                                callback_data=dic.get(list(dic)[i + mainf.event_index]))
+                markup_inline_back.add(bt)
+        if mainf.event_index > 0:
+            bt_b = types.InlineKeyboardButton("➡️", callback_data='next')
+            bt_p = types.InlineKeyboardButton("⬅️", callback_data='previous')
+            markup_inline_back.add(bt_p, bt_b)
+        else:
+            bt_b = types.InlineKeyboardButton("➡️", callback_data='next')
+            markup_inline_back.add(bt_b)
+        bot.edit_message_reply_markup(a.message.chat.id, a.message.message_id, reply_markup=markup_inline_back)
+
+    elif a.data == "back_main_menu":
+        markup_inline = types.InlineKeyboardMarkup()
+        bt_1 = types.InlineKeyboardButton(text='Театр🎭', callback_data='theatre')
+        bt_2 = types.InlineKeyboardButton(text='Кино🎬', callback_data='cinema')
+        bt_3 = types.InlineKeyboardButton(text='Концерты🎶', callback_data='concert')
+        bt_4 = types.InlineKeyboardButton(text='Выставки🖼', callback_data='show')
+        bt_5 = types.InlineKeyboardButton(text='Другие события🧐', callback_data='another')
+        markup_inline.add(bt_1, bt_2)
+        markup_inline.add(bt_3, bt_4)
+        markup_inline.add(bt_5)
+        bot.send_message(a.message.chat.id, "Посмотрите рекомендации в других категориях", reply_markup=markup_inline)
 
 
 
 
 
-bot.polling()
+bot.polling(none_stop=True)
--- mainf.py
+++ mainf.py
@@ -7,6 +7,9 @@ token_a = '1844668199:AAGqVvqCfa5a60vcEQ_Cm9dNMtuvRslDAXg'
 token_t = '1777347114:AAEow6W4NmD1r6OYCmKCtjMRth7olY9hOHk'
 
 querydata = None
+event_index = 0
+counter_update_photo = 0
+message_photo_id = None
 
 def bulka():
     muk = []
@@ -30,7 +33,19 @@ def chosen_concert(url):
     soup = BeautifulSoup(response.content, 'html.parser')
     adress = soup.find_all('div', class_="unit__col unit__col_oneandhalf")
     date = soup.find_all('h2', class_='_2YgOJ')
+    slogan = soup.find_all('h2', class_='info-widget__header')
+    name = soup.find_all('div', class_="_3kkV1 dbGiX")
     image = soup.find_all('img', class_='EVCML')
+    for data in name:
+        if soup.find_all('div', class_='_3kkV1 dbGiX'):
+            muk.append(data.text)
+    for data in slogan:
+        if soup.find_all('h2', class_='info-widget__header'):
+            delet = []
+            delet.append(data.text)
+            new_list = [word for line in delet for word in line.split()]
+            mes = " ".join(new_list)
+            muk.append(mes)
     for data in adress:
         if soup.find_all('div', class_='unit__col unit__col_oneandhalf'):
             muk.append(data.text)
@@ -39,7 +54,10 @@ def chosen_concert(url):
             muk.append(data.text)
 
 
-    return " ".join(muk)
+    message ="Концерт\n\n{}\n\n{}\n\n{}\n{}""".format(muk[0],muk[1],muk[2],muk[3])
+    message.replace('адрес','')
+
+    return message
 
 def chosen_photo(url):
     muk = None
